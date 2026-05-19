3d8056b774ff269fee697f5ca67f307cd6a5089c
anthony <ikennaanthony777@gmail.com>
2026-05-19 15:13:30 -0700

    sixth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Phone, MapPin } from "lucide-react";
 export default function Navbar() {
   return (
-    <div className="fixed top-0 right-0 z-50 w-full h-25  bg-black py-0">
+    <div className="fixed top-0 right-0 z-50 w-full h-20 md:h-25  bg-black py-0">
       <div className="mx-auto flex w-[95%] items-center justify-between px-6 py-1 md:py-0 md:px-12">
         {/* Logo */}
 
@@ -15,30 +15,30 @@ export default function Navbar() {
         <div className="flex flex-col gap-1 items-end">
           <a
             href="tel:+2348136495940"
-            className="  flex items-center gap-2
-                px-1 py-1 text-sm
+            className="  flex items-center gap-1
+                px-1 py-1 text-[12px]
                 md:px-4 md:py-2 md:text-base
                 bg-yellow-400 text-white
                 rounded-full
                 hover:bg-gray-800 transition"
           >
-            <Phone size={18} />
+            <Phone size={15} />
             Call Now
           </a>
 
-          <p className="text-white italic flex items-center gap-1 font-light text-sm">
+          <p className="text-white italic flex items-center gap-1 font-light text-sm pb-0">
             <a
               href="https://www.google.com/maps?q=Branch+2.0,+No.+6+Odenigbo+market,+Ohaozara+LGA,+Ebonyi+State"
               target="_blank"
               rel="noopener noreferrer"
               className=" flex items-center gap-1
-                px-1 py-1 text-[12px]
+                px-1 py-1 text-[11px]
                 md:px-4 md:py-2 md:text-base
                 bg-black text-white
                 rounded-full
                 hover:bg-gray-800 transition"
             >
-              <MapPin size={18} />
+              <MapPin size={16} />
               Our Location
             </a>
           </p>
